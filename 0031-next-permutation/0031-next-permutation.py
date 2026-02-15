e7082497a3fc7f9bc4bbb4f6839340c85768c32b
Nithin Reddy Challa <144381361+nchalla5@users.noreply.github.com>
2026-02-15 15:17:37 -0800

    Time: 0 ms (100%), Space: 19.3 MB (48.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,21 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i = len(nums)-1
-        # print(nums)
-        # nums[0:2].sort()
-        # print(nums)
         while i >= 0:
+            # I am trying to find out the first digit that not in descending order
             if i+1 == len(nums):
                 i -= 1
             elif (i+1 < len(nums) and nums[i] >= nums[i+1]):
                 i -= 1
             else:
+                # swap it with the first digit from end thats greater than it.
                 j = len(nums)-1
                 while j >= 0 and nums[j] <= nums[i]:
                     j -= 1
                 nums[i], nums[j] = nums[j], nums[i]
-                # print(nums)
-                # print(nums[i+1:][::-1])
+                # The remaining elements should be in descending order now
+                # So reverse all the elements after i to get the next perm in sequence
                 nums[i+1:] = nums[i+1:][::-1]
                 return 
         nums[:] = nums[::-1]
