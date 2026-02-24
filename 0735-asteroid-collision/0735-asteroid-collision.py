9f5f17fc662a3ece36c2ccb8641d24725c5165a8
Nithin Reddy Challa <144381361+nchalla5@users.noreply.github.com>
2026-02-23 17:20:20 -0800

    Time: 7 ms (59.72%), Space: 20.4 MB (29.74%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,30 +1,26 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         neg = []
+        pos = []
         i = 0
         while i < len(asteroids):
             # print(i)
             if asteroids[i] < 0:
-                j = i - 1
+                j = len(pos) - 1
                 while j >= 0:
-                    # print(j)
-                    if asteroids[j] > 0:
-                        if -asteroids[i] < asteroids[j]:
-                            break
-                        elif -asteroids[i] == asteroids[j]:
-                            asteroids[j] = 0
-                            break
-                        else:
-                            asteroids[j] = 0
+                    if -asteroids[i] < pos[j]:
+                        break
+                    elif -asteroids[i] == pos[j]:
+                        del pos[j]
+                        break
+                    else:
+                        del pos[j]
                     j -= 1
                 if j == -1:
                     neg.append(asteroids[i])
-                asteroids[i] = 0
+            else:
+                pos.append(asteroids[i])
             i += 1
-        pos = []
-        for ast in asteroids:
-            if ast != 0:
-                pos.append(ast)
         return neg+pos
 
 
