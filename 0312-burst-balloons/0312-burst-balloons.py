1b9b22f52f39aeba0ca806eefbc6f971fd91b2e9
Nithin Reddy Challa <144381361+nchalla5@users.noreply.github.com>
2026-03-09 22:09:57 -0700

    Time: 1824 ms (93.66%), Space: 22.4 MB (66.24%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,19 +1,31 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        def burst(i,j,nums, dp):
-            if i>j:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            sol = 0
-            for ind in range(i,j+1):
-                val = nums[i-1]*nums[ind]*nums[j+1] + burst(i,ind-1,nums,dp) + burst(ind+1,j,nums,dp)
-                if val > sol:
-                    sol = val
-            dp[i][j] = sol
-            return dp[i][j]
+        # def burst(i,j,nums, dp):
+        #     if i>j:
+        #         return 0
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     sol = 0
+        #     for ind in range(i,j+1):
+        #         val = nums[i-1]*nums[ind]*nums[j+1] + burst(i,ind-1,nums,dp) + burst(ind+1,j,nums,dp)
+        #         if val > sol:
+        #             sol = val
+        #     dp[i][j] = sol
+        #     return dp[i][j]
         n = len(nums)
-        dp = [[-1]*(n+1) for _ in range(n+1)]
+        dp = [[0]*(n+2) for _ in range(n+2)]
         nums = [1]+nums+[1]
-        return burst(1,n,nums, dp)
+        # return burst(1,n,nums, dp)
+
+        for i in range(n,0,-1):
+            for j in range(1,n+1):
+                if i > j:
+                    continue
+                sol = 0
+                for ind in range(i,j+1):
+                    val = nums[i-1]*nums[ind]*nums[j+1] + dp[i][ind-1] + dp[ind+1][j]
+                    if val > sol:
+                        sol = val
+                dp[i][j] = sol
+        return dp[1][n]
         
