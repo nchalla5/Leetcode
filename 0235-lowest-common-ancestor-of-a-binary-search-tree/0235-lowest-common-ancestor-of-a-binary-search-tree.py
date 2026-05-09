9c48eb2641d05a7d1ef5fcf6280e26cdc9339708
Nithin Reddy Challa <144381361+nchalla5@users.noreply.github.com>
2026-05-09 12:16:48 -0700

    Time: 71 ms (21.78%), Space: 22.6 MB (74.16%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,32 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def findPath(root, target, path):
-            if root is None or root.val == target:
-                return
-            if root.val < target:
-                path.append(root)
-                return findPath(root.right, target, path)
-            else:
-                path.append(root)
-                return findPath(root.left, target, path)
-        path1, path2 = [], []
-        findPath(root, p.val, path1)
-        path1.append(p)
-        findPath(root, q.val, path2)
-        path2.append(q)
-        sol = root.val
-        i = 0
-        while i < len(path1) and i < len(path2) and path1[i].val == path2[i].val:
-            i += 1
-        # print(path1, path2)
-        i -= 1
-        return path1[i]
+    def lowestCommonAncestor(self, root, p, q):
 
-        
+        current = root
+
+        while current:
+
+            # Both nodes lie in left subtree
+            if p.val < current.val and q.val < current.val:
+                current = current.left
+
+            # Both nodes lie in right subtree
+            elif p.val > current.val and q.val > current.val:
+                current = current.right
+
+            # Split point found -> this is LCA
+            else:
+                return current
